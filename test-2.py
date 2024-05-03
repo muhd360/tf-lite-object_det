c0827050331fa8b2d7f6bb2f3be3f4faf0512a7c
muhd360 <125988314+muhd360@users.noreply.github.com>
2024-05-03 03:50:44 -0400

    Add files via upload

---
+++ test-2.py
@@ -0,0 +1,192 @@
+import numpy as np
+import cv2
+import os
+import sys
+import glob
+import random
+import importlib.util
+from tensorflow.lite.python.interpreter import Interpreter
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+import pyttsx3
+#from .engine import Engine
+engine =pyttsx3.init()
+modelpath='detect.tflite'
+lblpath='labelmap.txt'
+min_conf=0.5
+cap=cv2.VideoCapture(-1,cv2.CAP_V4L)
+sys.path.append("/home/muhd/Desktop/TF-OBJ/models/research")
+
+#from object_detection.utils import label_map_util
+# from object_detection.utils import visualization_utils as vis_util
+
+
+# label_map = label_map_util.load_labelmap(lblpath)
+# categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=23, use_display_name=True)
+# category_index = label_map_util.create_category_index(categories)
+
+interpreter = Interpreter(model_path=modelpath)
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+float_input = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+with open(lblpath, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+while(True):
+    #print("hi")
+    def region_of_interest(img, vertices):
+        #print("entered")
+        mask = np.zeros_like(img)   
+        if len(img.shape) > 2:
+            channel_count = img.shape[2]
+            ignore_mask_color = (255,) * channel_count
+        else:
+            ignore_mask_color = 255   
+        cv2.fillPoly(mask, vertices, ignore_mask_color)
+        masked_image = cv2.bitwise_and(img, mask)
+        #print("works f9")
+        return masked_image
+
+    #video = cv2.VideoCapture(0)
+    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    # out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+    # out = cv2.VideoWriter('outpy.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (frame_width,frame_height))
+    #out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20 , (frame_width,frame_height))
+    try:
+
+    
+        while(cap.isOpened()):
+            #print("while condition entered")
+            ret, frame = cap.read()
+            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            imH, imW, _ = frame.shape
+            image_resized = cv2.resize(image_rgb, (width, height))
+
+            stime = time.time()
+            objects = []
+            class_str = ""
+            frame_width = image_resized.shape[0]
+            frame_height = image_resized.shape[1]
+            rows, cols = image_resized.shape[:2]
+            left_boundary = [int(cols*0.40), int(rows*0.95)]
+            left_boundary_top = [int(cols*0.40), int(rows*0.20)]
+            right_boundary = [int(cols*0.60), int(rows*0.95)]
+            right_boundary_top = [int(cols*0.60), int(rows*0.20)]
+            bottom_left  = [int(cols*0.20), int(rows*0.95)]
+            top_left     = [int(cols*0.20), int(rows*0.20)]
+            bottom_right = [int(cols*0.80), int(rows*0.95)]
+            top_right    = [int(cols*0.80), int(rows*0.20)]
+            vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
+            cv2.line(image_resized,tuple(bottom_left),tuple(bottom_right), (255, 0, 0), 5)
+            cv2.line(image_resized,tuple(bottom_right),tuple(top_right), (255, 0, 0), 5)
+            cv2.line(image_resized,tuple(top_left),tuple(bottom_left), (255, 0, 0), 5)
+            cv2.line(image_resized,tuple(top_left),tuple(top_right), (255, 0, 0), 5)
+            copied = np.copy(image_resized)
+            interested=region_of_interest(copied,vertices)
+
+
+            frame_expanded = np.expand_dims(interested, axis=0)
+                    # image_np_expanded = np.expand_dims(image_np, axis=0)
+            if float_input:
+                frame_expanded = (np.float32(frame_expanded) - input_mean) / input_std
+                    
+            interpreter.set_tensor(input_details[0]['index'], frame_expanded)
+            #print("input data being read")
+            interpreter.invoke()
+            # image_tensor = interpreter.get_tensor_by_name('image_tensor:0')
+            #boxes = interpreter.get_tensor_by_name('detection_boxes:0')
+            #     # Each score represent how level of confidence for each of the objects.
+            #     # Score is shown on the result image, together with the class label.
+            # scores = interpreter.get_tensor_by_name('detection_scores:0')
+            #classes = interpreter.get_tensor_by_name('detection_classes:0')
+            # num_detections = int(interpreter.get_tensor_by_name('num_detections:0'))
+            # Actual detection.
+            
+
+            #print(boxes)
+            #print(classes)
+            # (boxes, scores, classes, num) = sess.run(
+            #     [boxes, scores, classes, num_detections],
+            #     feed_dict={image_tensor: frame_expanded})
+            output_tensors = [interpreter.get_tensor(output_details['index']) for output_details in interpreter.get_output_details()]
+            scores, boxes, num_detections, classes= output_tensors            
+            print(scores.ndim, boxes.ndim, num_detections.ndim, classes.ndim)
+            print(boxes)
+            print(scores)
+            # vis_util.visualize_boxes_and_labels_on_image_array(
+            #     frame,
+            #     np.squeeze(boxes),
+            #     np.squeeze(classes).astype(np.int32),
+            #     np.squeeze(scores),
+            #     category_index,
+            #     use_normalized_coordinates=True,
+            #     line_thickness=8,
+            #     min_score_thresh=0.78)
+            print(frame_width,frame_height)
+            ymin = int((boxes[0][0][0]*frame_width))
+            print("ok till here")
+            xmin = int((boxes[0][0][1]*frame_height))
+            ymax = int((boxes[0][0][2]*frame_width))
+            xmax = int((boxes[0][0][3]*frame_height))
+            Result = np.array(image_resized[ymin:ymax,xmin:xmax])
+
+            ymin_str='y min  = %.2f '%(ymin)
+            ymax_str='y max  = %.2f '%(ymax)
+            xmin_str='x min  = %.2f '%(xmin)
+            xmax_str='x max  = %.2f '%(xmax)
+            cv2.putText(image_resized,ymin_str, (50, 50),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,0,0),2)
+            cv2.putText(image_resized,ymax_str, (50, 70),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,0,0),2)
+            cv2.putText(image_resized,xmin_str, (50, 90),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,0,0),2)
+            cv2.putText(image_resized,xmax_str, (50, 110),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,0,0),2)
+
+
+
+            print(scores.max())
+            print("left_boundary[0],right_boundary[0] :", left_boundary[0], right_boundary[0])
+            print("left_boundary[1],right_boundary[1] :", left_boundary[1], right_boundary[1])
+            print("xmin, xmax :", xmin, xmax)
+            print("ymin, ymax :", ymin, ymax)
+            if scores.max() > 0.3:
+                print("inif")
+                            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+       
+            if(xmin >= left_boundary[0]):
+                print("move LEFT - 1st !!!")
+                cv2.putText(image_resized,'Move LEFT!', (300, 100),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,255,0),2)
+                engine.say("move left")
+                engine.runAndWait()
+            elif(xmax <= right_boundary[0]):
+                
+                
+                print("move Right - 2nd !!!")
+                cv2.putText(image_resized,'Move RIGHT!', (300, 100),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,255,0),2)
+                engine.say("move right")
+                engine.runAndWait()
+            elif(xmin <= left_boundary[0] and xmax >= right_boundary[0]):
+                print("STOPPPPPP !!!! - 3nd !!!")
+                cv2.putText(image_resized,' STOPPPPPP!!!', (300, 100),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,255,0),2)
+                engine.say("stop")
+                engine.runAndWait()
+                time.sleep(2)
+                cv2.line(image_resized,tuple(left_boundary),tuple(left_boundary_top), (255, 0, 0), 5)
+                cv2.line(image_resized,tuple(right_boundary),tuple(right_boundary_top), (255, 0, 0), 5)
+
+            print("execution terminated")      
+            cv2.imshow('Frame', image_resized)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    except:
+        pass
--- test.py
+++ test.py
@@ -6,14 +6,16 @@ import glob
 import random
 import importlib.util
 from tensorflow.lite.python.interpreter import Interpreter
-
+import time
 import matplotlib
+import pyttsx3
 import matplotlib.pyplot as plt
-
+engine =pyttsx3.init()
 modelpath='detect.tflite'
 lblpath='labelmap.txt'
 min_conf=0.5
-cap=cv2.VideoCapture(-1,cv2.CAP_V4L)
+#cap=cv2.VideoCapture(-1,cv2.CAP_V4L)
+cap='/home/muhd/Pictures/Webcam/left.jpg'
 
 interpreter = Interpreter(model_path=modelpath)
 interpreter.allocate_tensors()
@@ -31,39 +33,71 @@ with open(lblpath, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
 while(True):
-    ret, frame =cap.read()
+    #ret, frame =cap.read()
+    frame=cv2.imread(cap)
     image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     imH, imW, _ = frame.shape
     image_resized = cv2.resize(image_rgb, (width, height))
+    stime = time.time()
+    objects = []
+    class_str = ""
+    frame_width = image_resized.shape[0]
+    frame_height = image_resized.shape[1]
+    rows, cols = image_resized.shape[:2]
+    left_boundary = [int(cols*0.40), int(rows*0.95)]
+    left_boundary_top = [int(cols*0.40), int(rows*0.20)]
+    right_boundary = [int(cols*0.60), int(rows*0.95)]
+    print(left_boundary[0])
+    print(f"right boundry",{right_boundary[0]})
+    right_boundary_top = [int(cols*0.60), int(rows*0.20)]
+    bottom_left  = [int(cols*0.20), int(rows*0.95)]
+    top_left     = [int(cols*0.20), int(rows*0.20)]
+    bottom_right = [int(cols*0.80), int(rows*0.95)]
+    top_right    = [int(cols*0.80), int(rows*0.20)]
+
+
+    line1_x = frame_width // 3
+    line2_x = 5 * (frame_width // 3)
+
+    # Draw the dividing lines
+    cv2.line(frame, (line1_x, 0), (line1_x, height*2), (255, 0, 0), 2)
+    cv2.line(frame, (line2_x, 0), (line2_x, height*2), (255, 0, 0), 2)
+    print("mememme")
+
+
     input_data = np.expand_dims(image_resized, axis=0)
     # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
     if float_input:
+
         input_data = (np.float32(input_data) - input_mean) / input_std
         
     # Perform the actual detection by running the model with the image as input
     interpreter.set_tensor(input_details[0]['index'],input_data)
     interpreter.invoke()
-    print("after invoke")
+    #print("after invoke")
     boxes = interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
     classes = interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
     scores = interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
-    print("after boxes")
-    print(boxes)
-    print(scores)
+    #print("after boxes")
+    #print(boxes)
+    #print(scores)
     detections = []
     
     
     for i in range(len(scores)):
-        print("inside scores")
+        #print("inside scores")
 
-        if ((scores[i] > 0) and (scores[i] <= 1.0)):
-            print("inside if statement")
+        if ((scores[i] > 0.5) and (scores[i] <= 1.0)):
+            #print("inside if statement")
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
             ymin = int(max(1,(boxes[i][0] * imH)))
             xmin = int(max(1,(boxes[i][1] * imW)))
             ymax = int(min(imH,(boxes[i][2] * imH)))
             xmax = int(min(imW,(boxes[i][3] * imW)))
+           #xmaxi=int(min(imW,(boxes[i][3] * imW)))
+            print("xmax",xmax)
+ 
             cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
             # Draw label
             object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
@@ -73,9 +107,37 @@ while(True):
             cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
             cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
             detections.append([object_name, scores[i], xmin, ymin, xmax, ymax])
+            if(object_name=="footpath"):
+                engine.say("climb the footpath")
+            elif(100<xmin <300):
+                print(label)
+                print("move R - 1st !!!")
+                cv2.putText(image_resized,'Move LEFT!', (300, 100),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,255,0),2)
+                engine.say("move left")
+                engine.runAndWait()
+                time.sleep(5.7)
+                print(xmax,xmin,ymax,ymin)
+            #elif(xmax <= right_boundary[0]):
+            elif(xmin >199):  
+                print(label)
+                
+                print("move Right - 2nd !!!")
+                cv2.putText(image_resized,'Move RIGHT!', (300, 100),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,255,0),2)
+                engine.say("move right")
+                engine.runAndWait()
+                time.sleep(5.7)
+                print(xmax,xmin,ymax,ymin)
+            elif(xmin<100):
+                print("STOPPPPPP !!!! - 3nd !!!")
+                cv2.putText(image_resized,' STOPPPPPP!!!', (300, 100),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,255,0),2)
+                engine.say("stop")
+                engine.runAndWait()
+                time.sleep(2)
+                cv2.line(image_resized,tuple(left_boundary),tuple(left_boundary_top), (255, 0, 0), 5)
+                cv2.line(image_resized,tuple(right_boundary),tuple(right_boundary_top), (255, 0, 0), 5)
+                print(xmax,xmin,ymax,ymin)
     
-    
-    
+    print(type(frame))
     cv2.imshow('output',frame)
     if((cv2.waitKey(1000) & 0xFF) == ord('q')):
         break
